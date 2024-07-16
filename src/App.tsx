fe30b89bbdded3acbae5b054558ad5ee1049b841
Giovanni Strasser <giostrasser@gmail.com>
2024-07-16 13:14:01 -0300

    Structures pages for further reference

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Router, Route, BrowserRouter, Routes } from 'react-router-dom';
+
 import MainPage from './pages/MainPage';
+import SettingsPage from './pages/SettingsPage';
+import QuizPage from './pages/QuizPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='*' element={<MainPage/>}></Route>
+        <Route path='/settings' element={<SettingsPage/>}></Route>
+        <Route path='quiz' element={<QuizPage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import './MainPage.css'
+import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
+  const navigate = useNavigate();
   return (
-    <div>MainPage</div>
+    <div className='page-root'>
+      <h1>Main Page</h1>
+      <a onClick={()=>(navigate("/settings"))}><h2>Opções</h2></a>
+      <a onClick={()=>(navigate("/editor"))}><h2>Criar</h2></a>
+      <a onClick={()=>(navigate("/quiz"))}><h2>Jogar</h2></a>
+    </div>
   )
 }
 
