104112af309e68137824d6ca4906fe7339e6952c
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-07-17 17:02:33 -0300

    Complete the basic design of the quiz page

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -8,15 +8,28 @@ function QuizPage() {
   return (
     <>
       <a onClick={()=>(navigate("/main"))}><img className='logo' src="mathmagik_logo.png" alt="Logotipo Mathmagik"/></a>
-      <h1>9 x 7</h1>
+
+      <div className='question'>
+        <h1>9 x 7</h1>
+      </div>
       <div className="btn">
         <div className='btn'>
-          <button className='quiz-button pink-button' onClick={()=>(navigate("/settings"))}>23</button>
-          <button className='quiz-button blue-button' onClick={()=>(navigate("/quiz"))}>27</button>
+          <button className='quiz-button pink-button'>23</button>
+          <button className='quiz-button blue-button'>27</button>
         </div>
         <div className='btn'>
-          <button className='quiz-button orange-button' onClick={()=>(navigate("/settings"))}>32</button>
-          <button className='quiz-button yellow-button' onClick={()=>(navigate("/quiz"))}>28</button>
+          <button className='quiz-button orange-button'>32</button>
+          <button className='quiz-button yellow-button'>28</button>
+        </div>
+      </div>
+
+      <div className="gap">
+        <div className='progress-bar-text'>
+          <div>00:23</div>
+          <div>23/70</div>
+        </div>
+        <div className='progress-bar-background'>
+          <div className='progress-bar-background bar'></div>
         </div>
       </div>
     </>
