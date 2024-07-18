f3373f63477d54c5475bbeb0177c7d72e14984f2
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-07-18 15:13:14 -0300

    Add more details to the page design

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -8,10 +8,11 @@ function SettingsPage() {
   return (
     <>
       <a onClick={()=>(navigate('/main'))}>
-        <img className='logo-settings' src='mathmagik_logo.png' alt='Logotipo Mathmagik'/>
+        <img className='logo logo-settings' src='mathmagik_logo.png' alt='Logotipo Mathmagik'/>
       </a>
 
       <h1>Opções</h1>
+      <div className="gap"></div>
       <div className='settings'>
         <h2>Timer</h2>
         <div className="timer">
@@ -28,47 +29,55 @@ function SettingsPage() {
       <div className='settings'>
         <h2>Dificuldade</h2>
         <div className='range'>
-          <div className='sliderValue'>
-            <span>100</span>
+          <div className="difficulty-text">
+            <div className='value left'>0</div>
+            <div className='value right'>10</div>
           </div>
           <div className='field'>
-            <div className='value left'>0</div>
-            <input type='range' min='0' max='200'></input>
-            <div className='value right'>200</div>
+            <input type='range' min='0' max='10'></input>
           </div>
-          
         </div>
       </div>
 
       <div className='settings'>
         <h2>Quantidade de questões</h2>
+        <div className='range'>
+          <div className="difficulty-text">
+            <div className='value left'>0</div>
+            <div className='value right'>200</div>
+          </div>
+          <div className='field'>
+            <input type='range' min='0' max='200'></input>
+          </div>
+        </div>
       </div>
 
       <div className='settings'>
         <h2>Tipos de questões</h2>
         <div className="question-types">
           <button className="timer-btn sum">
-            <img src="sum_blue.png" alt="adição"/>
+            <img src="subtraction_green.png" alt="contas de subtração"/>
           </button>
           <button className="timer-btn sum">
-            <img src="sum_blue.png" alt="adição"/>
+            <img src="division_purple.png" alt="contas de divisão"/>
           </button>
           <button className="timer-btn sum">
-            <img src="sum_blue.png" alt="adição"/>
+            <img src="sum_blue.png" alt="contas de soma"/>
           </button>
           <button className="timer-btn sum">
-            <img src="sum_blue.png" alt="adição"/>
+            <img src="multiplication_pink.png" alt="contas de multiplicação"/>
           </button>
           <button className="timer-btn sum">
-            <img src="sum_blue.png" alt="adição"/>
+            <img src="percentage_yellow.png" alt="contas de porcentagem"/>
           </button>
         </div>
       </div>
-
+      <div className="gap"></div>
       <div className='buttons'>
         <button className='button-std' onClick={()=>(navigate('/main'))}>MENU</button>
         <button className='button-std' onClick={()=>(navigate('/main'))}>SALVAR</button>
       </div>
+      <div className="gap"></div>
     </>
   )
 }
