53fa27f11293295a2f5075ef2fb917b6660dfa32
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-07-18 10:56:32 -0300

    Begin CSS for the settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function SettingsPage() {
+  const navigate = useNavigate();
+
   return (
-    <div>SettingsPage</div>
+    <>
+      <a onClick={()=>(navigate('/main'))}>
+        <img className='logo-settings' src='mathmagik_logo.png' alt='Logotipo Mathmagik'/>
+      </a>
+
+      <h1>Opções</h1>
+      <div className="settings">
+        <h2>Timer</h2>
+      </div>
+
+      <div className="settings">
+        <h2>Dificuldade</h2>
+      </div>
+
+      <div className="settings">
+        <h2>Quantidade de questões</h2>
+      </div>
+
+      <div className="settings">
+        <h2>Tipos de questões</h2>
+      </div>
+
+      <div className="buttons">
+        <button className='button-std' onClick={()=>(navigate('/quiz'))}>MENU</button>
+        <button className='button-std' onClick={()=>(navigate('/quiz'))}>SALVAR</button>
+      </div>
+    </>
   )
 }
 
