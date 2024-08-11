db2b7173cec76f392df06eb2afeecd5ca00d5ac3
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-08-11 19:12:46 -0300

    Finish initial implementation of score page

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -96,41 +96,18 @@ function QuizDisplay({ question, onQuestionDone }: Props) {
 
 }
 
-
-
 function ScoreDisplay() {
   const navigate = useNavigate();
-  const [totalTime, setTotalTime] = useState(10);
-  const [wrongQuestions, setWrongQuestions] = useState(10);
+  const [totalTime, setTotalTime] = useState(126); // Set useState to 0. 126 used for display purposes only. Total time must come from quiz page
+  const [wrongQuestions, setWrongQuestions] = useState(121); // Set useState to 0. 121 used for display purposes only. Wrog answers must come from quiz page
   const [questionQuantity, setQuestionQuantity] = useState(20); //get from settings
-
   const averageTimePerQuestion = totalTime / questionQuantity
   const errorPercentage = wrongQuestions / questionQuantity * 100
 
-  // useEffect(() => {//get from settings
-  //   const storageTime = localStorage.getItem("timer") || "20";
-  //   const storageDifficulty = localStorage.getItem("difficulty") || "3";
-  //   const storageQuestionQuantity = localStorage.getItem("questionQuantity")!;
-  //   const storageQuestionTypes = localStorage.getItem('questionTypes');
-  //   setTimer(parseInt(storageTime));
-  //   setDifficulty(parseInt(storageDifficulty));
-  //   setQuestionQuantity(parseInt(storageQuestionQuantity));
-  //   setQuestionTypes(storageQuestionTypes ? JSON.parse(storageQuestionTypes) : {
-  //     subtraction: false,
-  //     dividision: false,
-  //     sum: true,
-  //     multiplication: false
-  //   });
-
-  // }, []);
-
-
-  // waiting for quiz implementation
-  // useEffect(() => { //gets the data from localStorage (that must be defined on quiz page)
-  //   const totalTime = parseInt(localStorage.getItem("totalTime") || "10"); //get the total time (seconds) spent by the user to finish the quiz. Remove the || and change it to ! once the code is implemented on the quiz page.
-  //   const storageQuestionQuantity = parseInt(localStorage.getItem("questionQuantity") || "70"); //get the quantity of questions this quiz had. Remove the || and change it to ! once the code is implemented on the quiz page.
-  //   const wrongQuestions = parseInt(localStorage.getItem('wrongQquestions') || "0"); //get the amount of errors the user commited, or zero if none
-  // }, []);
+  useEffect(() => {
+    const storageQuestionQuantity = localStorage.getItem("questionQuantity")!;
+    setQuestionQuantity(parseInt(storageQuestionQuantity));
+  }, []);
 
   return (
     <div className="score-container">
@@ -150,7 +127,15 @@ function ScoreDisplay() {
         </div>
         <div className='details'>
           <div className='rectangle small'>
-            <div className='text blue'>{totalTime}</div>
+            <div className='text blue'>{Math.round(errorPercentage)}%</div>
+          </div>
+          <div>
+            <h2>de acerto</h2>
+          </div>
+        </div>
+        <div className='details'>
+          <div className='rectangle small'>
+            <div className='text orange'>{totalTime}</div>
           </div>
           <div>
             <h2>Tempo total</h2>
@@ -158,15 +143,7 @@ function ScoreDisplay() {
         </div>
         <div className='details'>
           <div className='rectangle small'>
-            <div className='text pink'>{errorPercentage}%</div>
-          </div>
-          <div>
-            <h2>de acertos</h2>
-          </div>
-        </div>
-        <div className='details'>
-          <div className='rectangle small'>
-            <div className='text orange'>{averageTimePerQuestion}s</div>
+            <div className='text green'>{Math.round(averageTimePerQuestion)}s</div>
           </div>
           <div>
             <h2>por questão</h2>
